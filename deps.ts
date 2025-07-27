8c3a84123ab55507aedf3c628a806e90eb850b7f
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-07-27 13:05:18 -0400

    update to get working with deno

--- deps.ts
+++ deps.ts
@@ -1,19 +1,9 @@
-// deps.ts - Centralized external dependencies using esm.sh for ESM compatibility
-// export { Vec3 } from "https://esm.sh/molstar@5.0.0-dev.0/lib/mol-math/linear-algebra/3d/vec3";
-// export { MVSData } from "https://esm.sh/molstar@5.0.0-dev.0/lib/extensions/mvs/mvs-data";
-// export type { Snapshot } from "https://esm.sh/molstar@5.0.0-dev.0/lib/extensions/mvs/mvs-data";
-
-// export {
-//   deflate,
-//   inflate,
-//   Zip,
-// } from "https://esm.sh/molstar@5.0.0-dev.0/lib/mol-util/zip/zip";
-
-export { Vec3 } from "npm:molstar@5.0.0-dev.0/lib/mol-math/linear-algebra/3d/vec3.js";
-export { MVSData } from "npm:molstar@5.0.0-dev.0/lib/extensions/mvs/mvs-data.js";
-export type { Snapshot } from "npm:molstar@5.0.0-dev.0/lib/extensions/mvs/mvs-data.js";
+// deps.ts - Centralized external dependencies using esm.sh with external dependencies
+export { Vec3 } from "https://esm.sh/molstar@5.0.0-dev.0/lib/mol-math/linear-algebra/3d/vec3?external=io-ts";
+export { MVSData } from "https://esm.sh/molstar@5.0.0-dev.0/lib/extensions/mvs/mvs-data?external=io-ts";
+export type { Snapshot } from "https://esm.sh/molstar@5.0.0-dev.0/lib/extensions/mvs/mvs-data?external=io-ts";
 export {
   deflate,
   inflate,
   Zip,
-} from "npm:molstar@5.0.0-dev.0/lib/mol-util/zip/zip.js";
+} from "https://esm.sh/molstar@5.0.0-dev.0/lib/mol-util/zip/zip?external=io-ts";
