698b11498b4cda78bc76a14475ce612c3cbeb264
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-07-31 12:13:46 -0400

    version bump

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-// deps.ts - Centralized external dependencies using esm.sh with external dependencies
-export { Vec3 } from "https://esm.sh/molstar@5.0.0-dev.0/lib/mol-math/linear-algebra/3d/vec3?external=io-ts";
-export { MVSData } from "https://esm.sh/molstar@5.0.0-dev.0/lib/extensions/mvs/mvs-data?external=io-ts";
-export type { Snapshot } from "https://esm.sh/molstar@5.0.0-dev.0/lib/extensions/mvs/mvs-data?external=io-ts";
+// deps.ts - Centralized external dependencies using esm.sh with external io-ts parameter
+export { Vec3 } from "https://esm.sh/molstar@5.0.0-dev.1/lib/mol-math/linear-algebra/3d/vec3";
+export { MVSData } from "https://esm.sh/molstar@5.0.0-dev.1/lib/extensions/mvs/mvs-data";
+export type { Snapshot } from "https://esm.sh/molstar@5.0.0-dev.1/lib/extensions/mvs/mvs-data";
 export {
   deflate,
   inflate,
   Zip,
-} from "https://esm.sh/molstar@5.0.0-dev.0/lib/mol-util/zip/zip?external=io-ts";
+} from "https://esm.sh/molstar@5.0.0-dev.1/lib/mol-util/zip/zip";
