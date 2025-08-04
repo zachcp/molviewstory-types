f5ffd920ed5bd718696535c54f38d7e9c07c9ea4
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-08-04 08:43:32 -0400

    bump

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 // deps.ts - Centralized external dependencies using npm: specifiers
-export { Vec3 } from "npm:molstar@5.0.0-dev.2/lib/mol-math/linear-algebra/3d/vec3.js";
-export { MVSData } from "npm:molstar@5.0.0-dev.2/lib/extensions/mvs/mvs-data.js";
-export type { Snapshot } from "npm:molstar@5.0.0-dev.2/lib/extensions/mvs/mvs-data.js";
+export { Vec3 } from "npm:molstar@5.0.0-dev.3/lib/mol-math/linear-algebra/3d/vec3.js";
+export { MVSData } from "npm:molstar@5.0.0-dev.3/lib/extensions/mvs/mvs-data.js";
+export type { Snapshot } from "npm:molstar@5.0.0-dev.3/lib/extensions/mvs/mvs-data.js";
 export {
   deflate,
   inflate,
   Zip,
-} from "npm:molstar@5.0.0-dev.2/lib/mol-util/zip/zip.js";
+} from "npm:molstar@5.0.0-dev.3/lib/mol-util/zip/zip.js";
